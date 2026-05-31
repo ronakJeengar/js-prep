92258aa7ae86313a8980093c67e5554d20f1a992
Ronak Jeengar <ronakjeengar7860@gmail.com>
2026-05-31 16:38:08 +0530

    vaiables in js

---
+++ variables.js
@@ -0,0 +1,28 @@
+const name = "Ronak";
+const age = 26;
+const country = "India";
+const isLearningJavascript = true;
+const favoriteLanguage = "Java";
+
+console.log("Name : ", name);
+console.log("Age : ", age);
+console.log("Country : ", country);
+console.log("Is Learning Javascript : ", isLearningJavascript);
+console.log("Favorite Language : ", favoriteLanguage);
+
+const productName = "Pen";
+const price = 10;
+let isInStock = true;
+
+console.log("product name is : ", productName);
+console.log("price is : ", price);
+console.log("In Stock : ", isInStock);
+
+isInStock = false;
+console.log("In Stock : ", isInStock);
+
+const userEmail = "ronak@exmaple.com";
+const userRole = "Admin";
+
+console.log("User Email : ", userEmail);
+console.log("User Role : ", userRole);
