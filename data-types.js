bf5cb903a38953c61ca19254392c06cf5fa14862
Ronak Jeengar <ronakjeengar7860@gmail.com>
2026-05-31 16:40:05 +0530

    data-types in js

---
+++ data-types.js
@@ -0,0 +1,11 @@
+const name = "ravi";
+let age = 24;
+let isStudent = true;
+let height;
+let isMarried = null;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof height);
+console.log(typeof isMarried);
