7bfb0c25687596bc3bf553dcde147a54d8b99a92
Ronak Jeengar <ronakjeengar7860@gmail.com>
2026-05-31 16:42:57 +0530

    loops in js

---
+++ loops.js
@@ -0,0 +1,9 @@
+const students = ["ronak", "ravi", "rahul", "shubham", "abhi"];
+
+for (let student = 0; student < students.length; student++) {
+  console.log(student, " -> ", students[student]);
+}
+
+console.log("Students Array Length : ", students.length);
+console.log("Fist Student : ", students[0]);
+console.log("Last Student : ", students[students.length - 1]);
