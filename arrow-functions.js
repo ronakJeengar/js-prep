251fdecf6e2c38ea6be72e142f54b20d8db2059d
Ronak Jeengar <ronakjeengar7860@gmail.com>
2026-05-31 16:47:04 +0530

    arrow-functions in js

---
+++ arrow-functions.js
@@ -0,0 +1,8 @@
+const greetUser = (name) => console.log(`Hello ${name}`);
+greetUser("Ronak");
+
+const multiplyNumbers = (a, b) => a * b;
+console.log(`Multiplication is : ${multiplyNumbers(3, 4)}`);
+
+const isEven = (a) => a % 2 === 0;
+console.log(`Is Even : ${isEven(3)}`);
